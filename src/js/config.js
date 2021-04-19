1e46ca7d7e22b97da0a0e9ed4f304357f023bf56
codydj <codydj13@hotmail.com>
2021-04-18 19:58:30 -0600

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ae498e58-d150-4e1c-a1f4-b6ab6bdf5a4c';
+export const MODAL_CLOSE_SEC = 2.5;
